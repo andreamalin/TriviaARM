7dd95f337ade4ca6ff04e5a0b96b400ec6653e11
andreamalin <andreaa.malin@gmail.com>
2020-05-27 10:50:41 -0600

    Add files via upload

---
+++ main.s
@@ -0,0 +1,6 @@
+/*Programa hecho por>> Andrea Amaya  Martin Amado        */
+/*            Carnet>> 19357         19020               */
+/*Programa en ARM para simular trivia crack
+	>PROYECTO FINAL ASSEMBLER							*/
+
+
